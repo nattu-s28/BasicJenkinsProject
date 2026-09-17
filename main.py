4c9bd372e008dd83840107bf3ae3db44f2216ec1
Natarajan <vnadarajan15@gmail.com>
2026-09-17 15:13:31 +0530

    Main Modified

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ def main():
     a = 10
     b = 10
 
+    print("-----------------")
     print("Addition:", add(a, b))
+    print("-----------------")
     print("Subtraction:", subtract(a, b))
+    print("-----------------")
     print("Multiplication:", multiply(a, b))
+    print("-----------------")
     print("Division:", divide(a, b))
+    print("-----------------")
 
 
 if __name__ == "__main__":
